50ced814e2fe9d8e5f1bdded59ccc36603176dc5
DESKTOP-8L5OUK3\LENOVO <tousifjamadar52a@gmail.com>
2025-02-03 09:56:45 +0530

    design updated

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -19,20 +19,29 @@ export default function Home() {
 
 
   return (
-    <div className="bg-red-100 min-h-screen p-10">
-    <h1 className="text-center py-5 md:py-10 text-3xl md:text-5xl font-bold bg-white text-red-400 border-2 border-red-300 border-dashed rounded-md">Match Tracker</h1>
+    <div className="bg-gradient-to-b from-red-100 to-red-200 min-h-screen flex flex-col items-center justify-center p-10">
+    <h1 className="text-center py-6 md:py-12 text-4xl md:text-6xl font-extrabold text-red-600 bg-white shadow-lg border-4 border-red-400 border-solid rounded-xl px-8 transition duration-300 hover:scale-105">🏆 Match Tracker ⚽</h1>
 
-   <div className="flex flex-col md:flex-row  justify-center md:justify-around md:mt-10">
-    <Matchcard score={scoreA} teamName="Team A" increaseScore={()=>{setScoreA(scoreA+1)}} decreaseScore={()=>{setScoreA(scoreA+1)}}/>
-    <Matchcard score={scoreB} teamName="Team B" increaseScore={()=>{setScoreB(scoreB+1)}} decreaseScore={()=>{setScoreB(scoreB+1)}}/>
+    <div className="flex flex-col md:flex-row justify-center items-center md:justify-around md:mt-10 mt-5 w-[800px] bg-gray-100 p-5">
+      <Matchcard
+        score={scoreA}
+        teamName="Team A"
+        increaseScore={() => setScoreA(scoreA + 1)}
+        decreaseScore={() => setScoreA(scoreA - 1)}
+      />
+      <Matchcard
+        score={scoreB}
+        teamName="Team B"
+        increaseScore={() => setScoreB(scoreB + 1)}
+        decreaseScore={() => setScoreB(scoreB - 1)}
+      />
+    </div>
 
- </div>
-
-   <div className='flex justify-center '>
-    <button className='bg-red-500 text-white py-2 px-4  my-8 rounded-md  text-xl shadow-md hover:bg-red-700 cursor-pointer' onClick={()=>{setScoreA(0);setScoreB(0);}}>
-      Reset Scores
-    </button>
-   </div>
- </div>	
+    <div className='flex justify-center '>
+        <button className='bg-red-500 text-white py-2 px-4 my-8 rounded-md text-xl shadow-md hover:bg-red-700 cursor-pointer' onClick={()=>{setScoreA(0);setScoreB(0);}}>
+          Reset Scores
+        </button>
+      </div>
+    </div>	
   )
 }
--- src/MatchCard/Match.js
+++ src/MatchCard/Match.js
@@ -4,7 +4,7 @@ import { CirclePlus as IconCirclePlus } from 'lucide-react'
 
 export default function Match({score,increaseScore,decreaseScore,teamName}) {
   return (
-    <div className="bg-white m-auto md:w-1/3 w-full mt-10 rounded-lg shadow-lg">
+    <div className="bg-white m-auto md:w-1/3 w-[500px] mt-5 rounded-lg shadow-lg">
     <h2 className="md:text-5xl text-4xl text-center my-4 font-bold text-red-400 pb-4 border-b-2">{teamName}</h2>
     <h3 className="text-center text-8xl text-red-400">{score}</h3>
     <div className="flex justify-around mb-5 md:mt-12">
